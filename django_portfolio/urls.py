5da35176580d297cdfb6907d599ea7eb87c3fd4a
Prashaant Dutt <pdutt04@gmail.com>
2020-08-02 15:00:02 +0530

    yoyo

--- django_portfolio/urls.py
+++ django_portfolio/urls.py
@@ -20,7 +20,7 @@ from django.conf.urls.static import static
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('projects/', include('projects.urls')),
+    path('/', include('projects.urls')),
 ]
 
 
